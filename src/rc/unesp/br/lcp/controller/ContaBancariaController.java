134d0d1e5cfa576f28b4965cb741f5ad0415f854
Bruno Farina <farinabrn@gmail.com>
2015-07-01 19:14:22 -0300

    Criação dos controllers

--- src/rc/unesp/br/lcp/controller/ContaBancariaController.java
+++ src/rc/unesp/br/lcp/controller/ContaBancariaController.java
@@ -6,10 +6,41 @@
 
 package rc.unesp.br.lcp.controller;
 
+import java.util.List;
+import rc.unesp.br.lcp.beans.BancoModel;
+import rc.unesp.br.lcp.beans.ContaBancariaModel;
+import rc.unesp.br.lcp.beans.SituacaoUsuarioModel;
+import rc.unesp.br.lcp.dao.ContaBancariaDAO;
+
 /**
  *
  * @author FARINA
  */
 public class ContaBancariaController {
     
+    private ContaBancariaDAO contaBancariaDAO = new ContaBancariaDAO();
+
+    public void adicionarContasBancarias(BancoModel banco, String agencia, String conta) {
+        ContaBancariaModel contaBancariaModel = new ContaBancariaModel(banco, agencia, conta);
+        contaBancariaDAO.adicionarContasBancarias(contaBancariaModel);        
+    }
+
+    public List<ContaBancariaModel> buscarContasBancarias() {
+        return contaBancariaDAO.buscarContasBancarias();  
+    }
+
+    public ContaBancariaModel buscarContaBancaria(BancoModel banco, String agencia, String conta) {
+        ContaBancariaModel contaBancariaModel = new ContaBancariaModel(banco, agencia, conta);
+        return contaBancariaDAO.buscarContaBancaria(contaBancariaModel);
+    }
+
+    public void alterarContaBancaria(BancoModel banco, String agencia, String conta) {
+        ContaBancariaModel contaBancariaModel = new ContaBancariaModel(banco, agencia, conta);
+        contaBancariaDAO.alterarContaBancaria(contaBancariaModel);        
+    }
+
+    public void apagarContaBancaria(BancoModel banco, String agencia, String conta) {
+        ContaBancariaModel contaBancariaModel = new ContaBancariaModel(banco, agencia, conta);
+        contaBancariaDAO.apagarContaBancaria(contaBancariaModel);
+    }
 }
--- src/rc/unesp/br/lcp/controller/ContaController.java
+++ src/rc/unesp/br/lcp/controller/ContaController.java
@@ -6,10 +6,40 @@
 
 package rc.unesp.br.lcp.controller;
 
+import java.util.List;
+import rc.unesp.br.lcp.beans.ContaModel;
+import rc.unesp.br.lcp.dao.ContaDAO;
+
 /**
  *
  * @author FARINA
  */
 public class ContaController {
+    
+    private ContaDAO contaDAO = new ContaDAO();
+
+    public void adicionarConta(String descricao, Double preco) {
+        ContaModel contaModel = new ContaModel(descricao, preco);
+        contaDAO.adicionarConta(contaModel);
+    }
+
+    public List<ContaModel> buscarContas() {
+        return contaDAO.buscarContas();
+    }
+
+    public ContaModel buscarConta(String descricao, Double preco) {
+        ContaModel contaModel = new ContaModel(descricao, preco);
+        return contaDAO.buscarConta(contaModel);        
+    }
+
+    public void alterarConta(String descricao, Double preco) {
+        ContaModel contaModel = new ContaModel(descricao, preco);
+        contaDAO.alterarConta(contaModel);        
+    }
+
+    public void apagarConta(String descricao, Double preco) {
+        ContaModel contaModel = new ContaModel(descricao, preco);
+        contaDAO.apagarConta(contaModel);        
+    }
     
 }
